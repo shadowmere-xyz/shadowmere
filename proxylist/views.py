452d8c181283f8fff27b6fa6d9b75b4c2c490cfe
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2021-10-22 14:30:32 +0200

    Order list by location

--- proxylist/views.py
+++ proxylist/views.py
@@ -4,4 +4,4 @@ from proxylist.models import Proxy
 
 
 def list_proxies(request):
-    return render(request, "index.html", {"proxy_list": Proxy.objects.filter(is_active=True)})
+    return render(request, "index.html", {"proxy_list": Proxy.objects.filter(is_active=True).order_by("location")})
