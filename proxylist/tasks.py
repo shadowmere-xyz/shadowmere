e36c567c8222229d09fcf4a4182e395b2330f3a8
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2021-10-22 16:32:44 +0200

    Add a pool of processes to be able to check concurrently

--- proxylist/tasks.py
+++ proxylist/tasks.py
@@ -1,12 +1,19 @@
 from proxylist.models import Proxy
 from proxylist.proxy import update_proxy_status
 from shadowmere.celery import app
+from multiprocessing.pool import Pool
+
+CONCURRENT_CHECKS = 10
 
 
 @app.task(bind=True)
 def update_status(self):
     print("Updating proxies status")
+    pool = Pool(processes=CONCURRENT_CHECKS)
+
     for proxy in Proxy.objects.all():
         print(f"\t>>>{proxy.url}")
-        update_proxy_status(proxy)
+        pool.apply_async(update_proxy_status, [proxy, ])
+
+    pool.terminate()
     print("Proxy status checked")
