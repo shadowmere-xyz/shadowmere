3cc2319b5639078b17493a21e8d32a741382a604
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2021-11-04 12:42:43 +0100

    Add application to import/export the proxy objects

--- proxylist/admin.py
+++ proxylist/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from proxylist.models import Proxy
 from proxylist.proxy import update_proxy_status
 
 
-class ProxyAdmin(admin.ModelAdmin):
+class ProxyResource(resources.ModelResource):
+    class Meta:
+        model = Proxy
+
+
+class ProxyAdmin(ImportExportModelAdmin):
     def update_status(modeladmin, request, queryset):
         for proxy in queryset:
             update_proxy_status(proxy)
@@ -14,6 +21,7 @@ class ProxyAdmin(admin.ModelAdmin):
     fields = ['url']
     actions = [update_status, ]
     list_filter = ('is_active', 'location',)
+    resource_class = ProxyResource
 
 
 admin.site.unregister(Group)
--- shadowmere/settings.py
+++ shadowmere/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'proxylist.apps.ProxylistConfig',
     'storages',
+    'import_export',
 ]
 
 MIDDLEWARE = [
