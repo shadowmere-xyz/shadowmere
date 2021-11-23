9d91fe0ce5de83a97bb98eb83eac894173e73aa5
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2021-11-23 20:47:55 +0100

    Use threaded update_status on manage

--- proxylist/management/commands/update_status.py
+++ proxylist/management/commands/update_status.py
@@ -2,11 +2,11 @@ from django.core.management.base import BaseCommand
 
 from proxylist.models import Proxy
 from proxylist.proxy import update_proxy_status
+from proxylist.tasks import update_status
 
 
 class Command(BaseCommand):
     help = 'Update the status of the proxy list'
 
     def handle(self, *args, **options):
-        for proxy in Proxy.objects.all():
-            update_proxy_status(proxy)
+        update_status()
