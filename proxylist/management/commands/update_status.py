be28d30b48b2679ab584c3424dda6561baacf27a
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2021-11-23 20:54:56 +0100

    Optimize imports for update_status command

--- proxylist/management/commands/update_status.py
+++ proxylist/management/commands/update_status.py
@@ -1,7 +1,5 @@
 from django.core.management.base import BaseCommand
 
-from proxylist.models import Proxy
-from proxylist.proxy import update_proxy_status
 from proxylist.tasks import update_status
 
 
