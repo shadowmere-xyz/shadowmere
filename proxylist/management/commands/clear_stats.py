29a8d1114df4543d316fdcbd4913e1620773be27
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2022-04-01 11:31:19 +0200

    Add command to dispose of statistics

---
+++ proxylist/management/commands/clear_stats.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+
+from proxylist.models import Proxy
+
+
+class Command(BaseCommand):
+    help = 'Clear all quality statistics'
+
+    def handle(self, *args, **options):
+        for proxy in Proxy.objects.all():
+            proxy.times_checked = 0
+            proxy.times_check_succeeded = 0
+            proxy.save()
