b04a47961f64ccf30a45e9407d473a29a85bb5a4
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2022-04-15 09:16:55 +0200

    Add a check before tests cycles to be sure the host is online
    this will avoid failing checks messing up with the stats only because the server went offline

--- proxylist/tasks.py
+++ proxylist/tasks.py
@@ -1,5 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
 
+import requests
+
 from proxylist.models import Proxy
 from proxylist.proxy import update_proxy_status
 from shadowmere.celery import app
@@ -11,7 +13,10 @@ CONCURRENT_CHECKS = 80
 def update_status(self):
     print("Updating proxies status")
 
-    with ThreadPoolExecutor(max_workers=CONCURRENT_CHECKS) as executor:
-        executor.map(update_proxy_status, Proxy.objects.all())
-
-    print("Proxy status checked")
+    req = requests.get("https://clients3.google.com/generate_204")
+    if req.status_code == 204:
+        with ThreadPoolExecutor(max_workers=CONCURRENT_CHECKS) as executor:
+            executor.map(update_proxy_status, Proxy.objects.all())
+        print("Proxy status checked")
+    else:
+        print("This host is having connection issues. Skipping test cycle.")
