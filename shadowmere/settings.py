527b58e8626422d1036d49aedb9606088036b5c6
Jorge Alberto Diaz Orozco (Akiel) <diazorozcoj@gmail.com>
2022-12-03 19:46:42 +0100

    Add sentry

--- shadowmere/settings.py
+++ shadowmere/settings.py
@@ -248,3 +248,23 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000
 
 CONN_MAX_AGE = 60
 CONN_HEALTH_CHECKS = True
+
+if not DEBUG and os.getenv("SENTRY_DSN") != "":
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=os.getenv("SENTRY_DSN"),
+        integrations=[
+            DjangoIntegration(),
+        ],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
