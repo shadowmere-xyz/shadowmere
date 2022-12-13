8a46bc6b6585dc28224e346b7c8e79ed0f128c12
Jorge Alberto Diaz Orozco (Akiel) <diazorozcoj@gmail.com>
2022-12-13 23:19:15 +0100

    Remove function to check for free ports
    this was a remain from the old checking implementation

--- proxylist/proxy.py
+++ proxylist/proxy.py
@@ -1,17 +1,7 @@
-import socket
-from contextlib import closing
-
 import requests
 from django.utils.timezone import now
 
 from shadowmere import settings
-
-
-def find_free_port():
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.bind(("", 0))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        return s.getsockname()[1]
 
 
 def get_proxy_location(proxy_url):
