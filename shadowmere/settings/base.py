d7cb42b78b1cfa0e7ed9ae39f335b0de14dd8c19
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2023-12-10 22:16:19 +0100

    fix: removed unused import

--- shadowmere/settings/base.py
+++ shadowmere/settings/base.py
@@ -11,12 +11,17 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.contrib.admin import AdminSite
 from django.utils.translation import gettext_lazy as _
 from pythonjsonlogger import jsonlogger
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -100,23 +105,7 @@ WSGI_APPLICATION = "shadowmere.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django_prometheus.db.backends.postgresql",
-            "NAME": "shadowmere",
-            "USER": "shadowmere",
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-            "HOST": "db",
-        }
-    }
+DATABASES = {"default": env.db()}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
