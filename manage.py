5b9ea89fe66ea8c6d8474c792fbedfc45e1b65cd
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2023-12-10 22:18:08 +0100

    feat: uisng envs

--- manage.py
+++ manage.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shadowmere.settings")
+    if os.getenv("DEBUG") == "True":
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shadowmere.settings.dev")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shadowmere.settings.prod")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
