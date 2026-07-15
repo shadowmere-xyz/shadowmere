90c26bc681da54c45fbf804307f5be605690c87d
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2026-07-15 09:56:58 +0000

    Revert "Fix huey dashboard"
    This reverts commit 346e0af3cea6b138d8fcd8e4ee1cb93537254a4b.

--- proxylist/apps.py
+++ proxylist/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 class ProxylistConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "proxylist"
-
-    def ready(self):
-        # Register tasks in the web process so the huey stats dashboard
-        # can list them; only run_huey autodiscovers the tasks module.
-        from . import tasks  # noqa: F401
